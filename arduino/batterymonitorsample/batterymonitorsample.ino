f98ddbdb025c7d2200212bd51d39a32eb1ef323a
jiyuhan <jiyu-han@uiowa.edu>
2017-10-28 10:40:18 -0500

    the bluetooth is working

---
+++ arduino/batterymonitorsample/batterymonitorsample.ino
@@ -0,0 +1,120 @@
+/*
+   Copyright (c) 2016 Intel Corporation.  All rights reserved.
+   See the bottom of this file for the license terms.
+*/
+
+/*
+ * Sketch: BatteryMonitor.ino
+ *
+ * Description:
+ *     This sketch example partially implements the standard Bluetooth
+ *   Low-Energy Battery service and connection interval paramater update.
+ *
+ *   For more information:
+ *     https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx
+ *
+ */
+
+#include <CurieBLE.h>
+
+
+
+BLEService batteryService("6e400001b5a3f393e0a9e50e24dcca9e"); // BLE Battery Service
+
+// BLE Battery Level Characteristic"
+BLEUnsignedCharCharacteristic batteryLevelChar("6e400003b5a3f393e0a9e50e24dcca9e",  // standard 16-bit characteristic UUID
+    BLEWrite | BLERead | BLENotify);     // remote clients will be able to
+// get notifications if this characteristic changes
+
+int oldBatteryLevel = 0;  // last battery level reading from analog input
+long previousMillis = 0;  // last time the battery level was checked, in ms
+
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  pinMode(13, OUTPUT);   // initialize the LED on pin 13 to indicate when a central is connected
+
+  
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet
+  */
+  BLE.setLocalName("BatteryMonitor");
+  BLE.setAdvertisedService(batteryService);  // add the service UUID
+  batteryService.addCharacteristic(batteryLevelChar); // add the battery level characteristic
+  BLE.addService(batteryService);   // Add the BLE Battery service
+  batteryLevelChar.setValue(oldBatteryLevel);   // initial value for this characteristic
+
+  // begin initialization
+  BLE.begin();
+
+
+  /* Start advertising BLE.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(13, HIGH);
+
+    // check the battery level every 200ms
+    // as long as the central is still connected:
+    while (central.connected()) {
+      long currentMillis = millis();
+      // if 200ms have passed, check the battery level:
+      if (currentMillis - previousMillis >= 200) {
+        previousMillis = currentMillis;
+        updateBatteryLevel();
+      }
+    }
+    // when the central disconnects, turn off the LED:
+    digitalWrite(13, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void updateBatteryLevel() {
+  /* Read the current voltage level on the A0 analog input pin.
+     This is used here to simulate the charge level of a battery.
+  */
+  int battery = analogRead(A0);
+  int batteryLevel = map(battery, 0, 1023, 0, 100);
+
+  Serial.print("Battery Level % is now: "); // print it
+  Serial.println(batteryLevel);
+  batteryLevelChar.setValue(batteryLevel);  // and update the battery level characteristic
+  oldBatteryLevel = batteryLevel;           // save the level for next comparison
+}
+
+/*
+   Copyright (c) 2016 Intel Corporation.  All rights reserved.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
---
+++ node-intermediate-server/device-read.js
@@ -0,0 +1,54 @@
+var noble = require('noble');
+const util = require('util');
+
+
+
+var peripheralId = 'f42e07780be14c878c360575c7d13e18';
+var serviceUuid = '6e400001b5a3f393e0a9e50e24dcca9e';
+var readUuid = '6e400003b5a3f393e0a9e50e24dcca9e';
+
+var readChar = null;
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+    // console.log("found: ", peripheral.advertisement.localName, ". and peripheral.id: ", peripheral.id);
+    // peripheral.id = "dfa4c6667ca14d778009d8675507813c";
+    // if(peripheral.advertisement.localName === "Concussion" || peripheral.advertisement.localName === "ARDUINO 101-348F") {
+    if(peripheral.id === peripheralId) {
+
+        peripheral.connect(function(error) {
+            if(error) console.log(error);
+            console.log("connected to", peripheral.advertisement.localName);
+            noble.stopScanning();
+
+            peripheral.discoverServices([serviceUuid], function(error, services) {
+                var myservice = services[0];
+                myservice.discoverCharacteristics(['6e400003b5a3f393e0a9e50e24dcca9e'], function(err, characteristics) {
+                    var mycharacteristic = characteristics[0];
+                    mycharacteristic.subscribe(function(error) {
+                        console.log("subscribed!");
+                        if(error) console.log(error);
+                    });
+                    // readChar = characteristic;
+                    console.log('saved readChar');
+                    // Handle data events for characteristic
+                    mycharacteristic.on('data', function(data, isNotification) {
+                       console.log('Sensor Value', data.readUInt8(0));
+                    });
+                });
+            });
+        });
+
+        peripheral.once("disconnect", function() {
+            console.log("disconneted");
+            process.exit(0);
+        });
+    }
+});
